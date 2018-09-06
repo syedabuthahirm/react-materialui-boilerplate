9d4f064cf3721db76bdb048e2dd1ef39c8a4611f
syedabuthahirm <abuthahir741@gmail.com>
2018-09-06 16:14:47 +0530

    Added React Router Multiple Layouts

---
+++ client/src/layouts/EmptyLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EmptyLayout = props => {
+  return <div>{props.children}</div>;
+};
+
+export default EmptyLayout;
---
+++ client/src/layouts/MainLayout.js
@@ -0,0 +1,62 @@
+import React, { Fragment, Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
+
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    marginLeft: theme.spacing.unit * 9,
+    padding: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 7,
+    overflowX: "hidden"
+  },
+  contentShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  }
+});
+
+class MainLayout extends Component {
+  state = {
+    open: false
+  };
+
+  handleToggleDrawer = () => {
+    this.setState(prevState => {
+      return { open: !prevState.open };
+    });
+  };
+
+  render() {
+    const { classes, children } = this.props;
+    return (
+      <Fragment>
+        <div className={classes.root}>
+          <Header handleToggleDrawer={this.handleToggleDrawer} />
+          <main
+            className={classNames(classes.content, {
+              [classes.contentShift]: this.state.open
+            })}
+          >
+            {children}
+          </main>
+        </div>
+        <Sidebar open={this.state.open} drawerWidth={drawerWidth} />
+      </Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(MainLayout);
