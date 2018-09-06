86ba324887e5ac1df66c72475ce37ef82a2093a9
syedabuthahirm <abuthahir741@gmail.com>
2018-09-06 16:20:40 +0530

    Upated README and app.js

--- README.md
+++ README.md
@@ -1,25 +1,31 @@
 # React Material UI Boilerplate
-  Starter kit for react and material UI. client side was created with [create-react-app](https://github.com/facebook/create-react-app) server side was backed with [Express JS](http://expressjs.com/).
-  
+
+Starter kit for react and material UI. client side was created with [create-react-app](https://github.com/facebook/create-react-app) server side was backed with [Express JS](http://expressjs.com/).
+
 ## Features
-  - Material UI (https://material-ui.com/)
-  - Redux (https://redux.js.org/)
-  - Express (http://expressjs.com/)
-  
+
+- Material UI (https://material-ui.com/)
+- Redux (https://redux.js.org/)
+- Express (http://expressjs.com/)
+- React Router (https://reacttraining.com/react-router/web/guides/philosophy)
+
 ## Todos
-  - [ ] Different types of work environment setup ( stage, dev, prod )
-  - [ ] Server Side API integration example
-  
+
+- [ ] Different types of work environment setup ( stage, dev, prod )
+- [ ] Server Side API integration example
+
 ## Installation
-  - Clone a repo using `git clone https://github.com/syedabuthahirm/react-materialui-boilerplate.git`
-  - Navigate to client folder `cd client`
-  - Install node modules using `npm install` or `yarn install`
-  - Naviagte to server folder `cd server`
-  - Install node modules using `npm install` or `yarn install`
-  - Start a client app `cd client && npm run start`
-  - Start a server app `cd server && npm run start`
-  
+
+- Clone a repo using `git clone https://github.com/syedabuthahirm/react-materialui-boilerplate.git`
+- Navigate to client folder `cd client`
+- Install node modules using `npm install` or `yarn install`
+- Naviagte to server folder `cd server`
+- Install node modules using `npm install` or `yarn install`
+- Start a client app `cd client && npm run start`
+- Start a server app `cd server && npm run start`
+
 ## Get Involved
+
 - Clone or fork the project.
 - Create feature branches off develop branch.
 - Once your changes are ready create a pull request into the master branch.
--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,87 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { withStyles } from "@material-ui/core/styles";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
 import Home from "./containers/Home";
 import Setting from "./containers/Setting";
+import MainLayout from "./layouts/MainLayout";
+import EmptyLayout from "./layouts/EmptyLayout";
 
-const drawerWidth = 240;
+const Signin = props => {
+  return <div>Sign in</div>;
+};
 
-const styles = theme => ({
-  root: {
-    display: "flex"
-  },
-  content: {
-    flexGrow: 1,
-    marginLeft: theme.spacing.unit * 9,
-    padding: theme.spacing.unit * 3,
-    marginTop: theme.spacing.unit * 7,
-    overflowX: "hidden"
-  },
-  contentShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  }
-});
+const NotFound = () => {
+  return <div>NotFound</div>;
+};
+
+const DashboardRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={matchProps => (
+        <MainLayout>
+          <Component {...matchProps} />
+        </MainLayout>
+      )}
+    />
+  );
+};
+
+const EmptyRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={matchProps => (
+        <EmptyLayout>
+          <Component {...matchProps} />
+        </EmptyLayout>
+      )}
+    />
+  );
+};
 
 class App extends Component {
   state = {
-    open: false
-  };
-
-  handleToggleDrawer = () => {
-    this.setState(prevState => {
-      return { open: !prevState.open };
-    });
+    auth: true
   };
 
   render() {
-    const { classes, settings } = this.props;
+    const { settings } = this.props;
 
     return (
       <MuiThemeProvider theme={settings.theme}>
         <CssBaseline />
         <Router>
-          <React.Fragment>
-            <div className={classes.root}>
-              <Header handleToggleDrawer={this.handleToggleDrawer} />
-              <main
-                className={classNames(classes.content, {
-                  [classes.contentShift]: this.state.open
-                })}
-              >
-                <Route exact path="/" component={Home} />
-                <Route path="/setting" component={Setting} />
-              </main>
-            </div>
-            <Sidebar open={this.state.open} drawerWidth={drawerWidth} />
-          </React.Fragment>
+          {this.state.auth ? (
+            <Switch>
+              <DashboardRoute path="/dashboard" component={Home} />
+              <DashboardRoute path="/setting" component={Setting} />
+              <Route path="/signin" render={() => <Redirect to="/" />} />
+              <DashboardRoute exact path="/" component={Home} />
+              <EmptyRoute component={NotFound} />
+            </Switch>
+          ) : (
+            <Switch>
+              <EmptyRoute path="/signin" component={Signin} />
+              <Redirect to="/signin" />
+            </Switch>
+          )}
         </Router>
       </MuiThemeProvider>
     );
   }
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired
-};
+App.propTypes = {};
 
 const mapStateToProps = state => {
   return {
@@ -86,4 +92,4 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   null
-)(withStyles(styles)(App));
+)(App);
